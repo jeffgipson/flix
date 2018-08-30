597facfdf465cbf3ffc52b800a09e2fcab103d61
jeffgipson <jeff@recruiterswebsites.com>
2018-08-29 22:43:45 -0500

    update

---
+++ public/assets/closeform-8f81b7c975a2cb663c9b0de1d0feba0a79883438beb2b0e2cd1c1909630f5739.js
@@ -0,0 +1,1 @@
+$("#submit_review").on("submit",function(){$("#exampleModalCenter").modal("hide")}),$(document).ready(function(){-1!=window.location.href.indexOf("#exampleModalCenter")&&$("#exampleModalCenter").modal("show")});
